4ba5351544ef85a963950c39ca1bbfcbcc43cf86
Muhammadullo <mnastulloev@gmail.com>
2023-12-13 09:56:43 +0500

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -169,7 +169,7 @@ function App() {
 
 
         {/*# card8 */}
-        <div className='w-[100%] h-[70vh] border-[2px] border-red-700 border-solid flex flex-col justify-center items-center'>
+        <div className='w-[100%] h-[70vh]  flex flex-col justify-center items-center'>
           <p className='text-[38px] m-[10px] font-bold text-[#192252]'>Hear From Our Happy Customers</p>
           <img src={card8} className='m-[10px]' />
           <p className='text-center text-[#848FAC]'>“Great service, great food, great people. The scenery is also beautiful, you can do so much <br /> activity even with your famility. Worth every penny! you should come and see for your self.  5 <br /> out of 5! the waffle is great!”</p>
